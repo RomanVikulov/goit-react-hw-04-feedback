5b14dba815ff784b8b23b64761d73651fb402502
Roman <romanuch06.88@gmail.com>
2022-10-30 19:48:34 +0200

    Create Section.styled.js

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    padding: 30px;
+`;
+
+const Title = styled.h1`
+    margin-bottom: 20px;
+    font-size: 32px;
+    font-weight: 700;
+    color: rgb(0, 0, 0);
+`;
+
+export { Section, Title };
