e4d8cb020f780b8c60945082b0566d53494ee46a
Roman <romanuch06.88@gmail.com>
2022-10-30 16:28:43 +0200

    Create Container.styled.js

---
+++ src/components/base/Container.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    width: 400px;
+    margin: auto;
+    border-radius: 5px;
+    box-shadow: 5px 5px 10px -2px rgba(100, 100, 100, 0.5);
+`;
